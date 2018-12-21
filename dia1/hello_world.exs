e1cf8ac8314e64ed3d47c83f37e4de3a0b0e8bbf
LuisSas <luis@makingdevs.com>
2018-12-21 11:33:48 -0600

    Adding piping function

--- dia1/hello_world.exs
+++ dia1/hello_world.exs
@@ -5,10 +5,6 @@ defmodule Hello do
   def print(:fr), do: "Bonjour!!"
   def print(:ch), do: "你好!!"
 
-  def some(x \\ 1) do
-    x
-  end
-
   def fib(0), do: 0
   def fib(1), do: 1
   def fib(n), do: fib(n-2) + fib(n-1)
@@ -16,14 +12,18 @@ defmodule Hello do
   def count(list), do: counting(list, 0)
   defp counting([], counter), do: counter
   defp counting([_h|t], counter), do: counting(t, counter + 1)
+
+  def piping(l) do
+    l
+    |> Enum.map(fn x -> x*3 end)
+    |> Enum.filter(fn x -> x > 15 end)
+    |> Enum.filter(fn x -> rem(x,2) == 0 end)
+  end
 end
 
 IO.puts(Hello.print())
 IO.puts(Hello.print(:es))
 IO.puts(Hello.print(:fr))
 IO.puts(Hello.print(:ch))
-
-##IO.puts(Hello.some())
-##IO.puts(Hello.some(5))
 
 IO.puts(Hello.fib(10))
