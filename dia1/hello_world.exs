304fe8dabe3c205ddc9e392537d0a7fda7ea9b54
LuisSas <luis@makingdevs.com>
2018-12-21 10:06:31 -0600

    Adding 'some' function that receives a number or put a default number, and add chinese characters in a function

--- dia1/hello_world.exs
+++ dia1/hello_world.exs
@@ -2,7 +2,17 @@ defmodule Hello do
   def print(lang \\ :en)
   def print(:en), do: "Hello World!!"
   def print(:es), do: "Hola Inmundo!!"
+  def print(:fr), do: "Bonjour!!"
+  def print(:ch), do: "你好!!"
+
+  def some(x \\ 1) do
+    x
+  end
 end
 
 IO.puts(Hello.print())
 IO.puts(Hello.print(:es))
+IO.puts(Hello.print(:fr))
+IO.puts(Hello.print(:ch))
+IO.puts(Hello.some())
+IO.puts(Hello.some(5))
