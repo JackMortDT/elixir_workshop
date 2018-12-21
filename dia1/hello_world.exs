06d08e1c00994bd2343f508df624241775cd7851
LuisSas <luis@makingdevs.com>
2018-12-21 10:13:29 -0600

    Adding fib function that calculate the fibonacci of a number

--- dia1/hello_world.exs
+++ dia1/hello_world.exs
@@ -8,11 +8,18 @@ defmodule Hello do
   def some(x \\ 1) do
     x
   end
+
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n), do: fib(n-2) + fib(n-1)
 end
 
 IO.puts(Hello.print())
 IO.puts(Hello.print(:es))
 IO.puts(Hello.print(:fr))
 IO.puts(Hello.print(:ch))
-IO.puts(Hello.some())
-IO.puts(Hello.some(5))
+
+##IO.puts(Hello.some())
+##IO.puts(Hello.some(5))
+
+IO.puts(Hello.fib(10))
