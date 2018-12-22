3c668eddc14530fc44e49773aed812e512fbe823
LuisSas <luis@makingdevs.com>
2018-12-22 09:27:12 -0600

    Change string for atom type

--- dia1/Fizz_Buzz.exs
+++ dia1/Fizz_Buzz.exs
@@ -8,11 +8,13 @@ defmodule Fizz_Buzz do
   defp do_fizz_buzz(n) do
     (rem(n,3) == 0 and rem(n,5) == 0) && fizz_and_buzz(3,5) ||
     (rem(n,3) == 0) && fizz_and_buzz(3) ||
-    (rem(n,5) == 0) && fizz_and_buzz(5)
+    (rem(n,5) == 0) && fizz_and_buzz(5) ||
+    fizz_and_buzz(n)
   end
 
-  defp fizz_and_buzz(3,5), do: "FizzBuzz"
-  defp fizz_and_buzz(3), do: "Fizz"
-  defp fizz_and_buzz(5), do: "Buzz"
+  defp fizz_and_buzz(3,5), do: :FizzBuzz
+  defp fizz_and_buzz(3), do: :Fizz
+  defp fizz_and_buzz(5), do: :Buzz
+  defp fizz_and_buzz(n), do: n
 
 end
