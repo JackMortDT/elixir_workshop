05dbffbf3520a25838dc363b659452b47586582c
LuisSas <luis@makingdevs.com>
2018-12-22 10:33:59 -0600

    Add Pattern matching in functions with lists, binary, maps

--- dia2/greeter.ex
+++ dia2/greeter.ex
@@ -1,11 +1,21 @@
 defmodule Greeter do
 
-  def hello(%{name: name, lastname: lastname} = person) do
-    IO.puts("Hello #{name} #{lastname}")
+  def hello(names) when is_list(names) do
+    names
+    |> Enum.join(", ")
+    |> phrase
   end
-  def hello(%{name: name} = person) do
-    IO.puts "Hello #{name}"
-    IO.inspect person
+  def hello(name) when is_binary(name) do
+    phrase(name)
   end
+  def hello(%{name: name, lastname: lastname} = _person) do
+    phrase(name, lastname)
+  end
+  def hello(%{name: name} = _person) do
+    phrase(name)
+  end
+
+  defp phrase(name, lastname), do: "Hello #{name} #{lastname}"
+  defp phrase(name), do: "Hello #{name}"
 
 end
