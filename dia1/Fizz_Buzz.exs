5604de373f1527051d297e60f87305a8e8e0d770
LuisSas <luis@makingdevs.com>
2018-12-21 22:14:12 -0600

    Using patern matching to print the messages

--- dia1/Fizz_Buzz.exs
+++ dia1/Fizz_Buzz.exs
@@ -5,10 +5,14 @@ defmodule Fizz_Buzz do
     |> Enum.map( fn x -> {x, do_fizz_buzz(x)} end )
   end
 
-  def do_fizz_buzz(n) do
-    (rem(n,3) == 0 and rem(n,5) == 0) && "FizzBuzz" ||
-    (rem(n,3) == 0) && "Fizz" ||
-    (rem(n,5) == 0) && "Buzz"
+  defp do_fizz_buzz(n) do
+    (rem(n,3) == 0 and rem(n,5) == 0) && fizz_and_buzz(3,5) ||
+    (rem(n,3) == 0) && fizz_and_buzz(3) ||
+    (rem(n,5) == 0) && fizz_and_buzz(5)
   end
+
+  defp fizz_and_buzz(3,5), do: "FizzBuzz"
+  defp fizz_and_buzz(3), do: "Fizz"
+  defp fizz_and_buzz(5), do: "Buzz"
 
 end
