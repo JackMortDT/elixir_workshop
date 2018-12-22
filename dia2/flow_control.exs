dc709e3774b94ecfa31c1f7bdc86ad36c80fcd03
LuisSas <luis@makingdevs.com>
2018-12-22 09:50:23 -0600

    Adding cond structure

--- dia2/flow_control.exs
+++ dia2/flow_control.exs
@@ -7,8 +7,17 @@ end
 IO.puts result
 
 tuple = {:ok, 123}
-tuple2 = {:ok, "hello"}
 
+result = case tuple do
+  {:ok, result} when is_float(result) -> result * 2
+  {:ok, result} when is_binary(result) -> result
+  {:error, reason} -> "Error #{reason} "
+  _ -> "Catch all"
+end
+
+IO.puts result
+
+tuple2 = {:ok, "hello"}
 result = case tuple2 do
   {:ok, result} when is_float(result) -> result * 2
   {:ok, result} when is_binary(result) -> result
@@ -18,8 +27,8 @@ end
 
 IO.puts result
 
-file = File.read(System.get_env("HOME") <> "/.gitconfig")
 
+file = File.read(System.get_env("HOME") <> "/.gitconfig")
 result = case file do
   {:ok, text} -> String.length(text)
   {:error, reason} -> "Can't read because #{reason}"
@@ -28,7 +37,16 @@ end
 
 IO.puts result
 
+
 case :even do
   :odd -> "Odd"
   _ -> "Unrecheable case"
 end
+
+result = cond do
+  2 + 2 == 5 -> "Not true"
+  2 * 2 == 3 -> "Neither"
+  1 + 1 == 2 -> "Truthy"
+  true ->  "No way"
+end
+IO.puts result
