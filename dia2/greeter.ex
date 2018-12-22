804fb2918c91cbcbca5f73de5b0e5a519fc48d44
LuisSas <luis@makingdevs.com>
2018-12-22 10:23:33 -0600

    Adding pattern matching in hello function that receives lastname

--- dia2/greeter.ex
+++ dia2/greeter.ex
@@ -1,6 +1,11 @@
 defmodule Greeter do
+
+  def hello(%{name: name, lastname: lastname} = person) do
+    IO.puts("Hello #{name} #{lastname}")
+  end
   def hello(%{name: name} = person) do
     IO.puts "Hello #{name}"
     IO.inspect person
   end
+
 end
